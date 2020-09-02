a4fa815b50c1359f194e09a18a7b703423d02a7c
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-09-02 15:18:30 +0100

    gatsby-node.js - tell webpack to ignore emacs lock files
    Else it crashes rather ungracefully. These files are symlinks to
    nowhere and stat'ing them fails.
    
    Also ignore node_modules, and tilda backup files

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,21 @@
  */
 
 // You can delete this file if you're not using it
+
+
+// Tell webpack to ignore emacs lock files (else it crashes!)
+exports.onCreateWebpackConfig = ({
+  stage,
+  rules,
+  loaders,
+  plugins,
+  actions,
+}) => {
+  actions.setWebpackConfig({
+    devServer: {
+      watchOptions: {
+        ignored: /\.#|node_modules|~$/,
+      },
+    }
+  })
+};
