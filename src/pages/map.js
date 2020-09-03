504ac1fb5b6308fb778ab1c0de6e74b490cf5221
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-09-03 15:56:49 +0100

    src/pages/map.js - adjust map to fit into layout
    We use AspectRatio

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,7 +1,21 @@
 import React from 'react';
-
+import { Link } from "gatsby";
+import Layout from "../components/layout";
+import AspectRatio from 'react-aspect-ratio';
+import SEO from "../components/seo";
 import Map from '../components/Map';
+import 'react-aspect-ratio/aspect-ratio.css';
 
-const MapPage = () => <Map />;
+const MapPage = () => (
+    <Layout>
+    <SEO title="Map page" />
+    <h1>Hi from the map page</h1>
+    <p>Welcome to the map page</p>
+    <AspectRatio ratio="560/315" style={{ width: '100%' }}>
+    <Map />
+    </AspectRatio>
+    <Link to="/">Go back to the homepage</Link>
+    </Layout>
+);
 
 export default MapPage;
