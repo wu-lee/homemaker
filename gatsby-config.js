48100eadb402aefa7531719b203e565cb709035e
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-09-04 10:47:01 +0100

    Set the title, put map on front page, trim out boilerplate pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Homemaker Oxford`,
+    description: `A report on homelessness in Oxford, by Transition By Design`,
+    author: `@transitionbyd`,
   },
   pathPrefix: "/homemaker",
   plugins: [
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,65 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from "gatsby";
+import Layout from "../components/layout";
+import AspectRatio from 'react-aspect-ratio';
+import SEO from "../components/seo";
+import Map from '../components/Map';
+import { Marker, Popup } from "react-leaflet";
+import rar from 'react-aspect-ratio/aspect-ratio.css';
+import data from "../data.json";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+console.log(rar+''); // work around overzealous dependency pruning
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const mapSettings = {
+    center: [51.7522, -1.2560],
+    zoom: 12,
+};
+
+const IndexPage = () => {
+    const [activePark, setActivePark] = React.useState(null);
+
+    return (
+    <Layout>
+        <SEO title="Homemaker Oxford project" />
+        <h1>Homemaker Oxford demo</h1>
+        <p>
+	This is just a fairly trivial proof-of-concept of a map with some pins on it.
+        A descriptions could go here. There could be links, and more pages.
+	</p>
+        <AspectRatio ratio="560/315" style={{ width: '100%' }}>
+            <Map settings={mapSettings}>
+                {data.features.map(park => (
+                    <Marker
+                        key={park.properties.PARK_ID}
+                        position={[
+                            park.geometry.coordinates[0],
+                            park.geometry.coordinates[1]
+                        ]}
+                        onClick={() => {
+                            setActivePark(park);
+                        }}
+                    />
+                ))}
+                {activePark && (
+                    <Popup
+                        position={[
+                            activePark.geometry.coordinates[0]+0.005,
+                            activePark.geometry.coordinates[1]
+                        ]}
+                        onClose={() => {
+                            setActivePark(null);
+                        }}
+                    >
+                        <div>
+                            <h2>{activePark.properties.NAME}</h2>
+                            <p>{activePark.properties.DESCRIPTION}</p>
+                        </div>
+                    </Popup>
+                )}
+            </Map>
+        </AspectRatio>
+    </Layout>
+    )
+}
 
 export default IndexPage
